73a50ad667acdd82f4ab536f56e5925cd71c3fe6
Вадим Гермашев <124063584+VadimGera@users.noreply.github.com>
2023-08-26 17:36:59 +0300

    закинул

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    [SerializeField] private GameObject Ground;
+
+    public void OnTriggerEnter(Collider other)
+    {
+       
+    }
+}
